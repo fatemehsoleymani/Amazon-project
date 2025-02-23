094bef52af7a2f96e193d6ac32a2de9e35636161
fatemeh <fatemehsoleymani90@yahoo.com>
2025-02-23 15:58:15 -0500

    using closure solutions for duble clicking on add to cart

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -57,6 +57,23 @@ products.forEach((product) => {
 document.querySelector('.js-products-grid').innerHTML = productHTML;
 
 document.querySelectorAll('.js-add-to-cart').forEach((button) => {
+  // closure. Each time we run the loop, it will create
+  // a new variable called addedMessageTimeoutId and do
+  // button.addEventListener().
+  //
+  // Then, because of closure, the function we give to
+  // button.addEventListener() will get a unique copy
+  // of the addedMessageTimeoutId variable and it will
+  // keep this copy of the variable forever.
+  // (Reminder: closure = if a function has access to a
+  // value/variable, it will always have access to that
+  // value/variable).
+  //
+  // This allows us to create many unique copies of the
+  // addedMessageTimeoutId variable (one for every time
+  // we run the loop) so it lets us keep track of many
+  // timeoutIds (one for each product).
+  let addedMessageTimeoutId;
   button.addEventListener('click', () => {
     const {productId} = button.dataset;
 
@@ -93,8 +110,16 @@ document.querySelectorAll('.js-add-to-cart').forEach((button) => {
 
     addedMessage.classList.add('added-to-cart-visible');
 
-    setTimeout(() => {
-      addedMessage.classList.remove('added-to-cart-visible');
+    // Check if a previous timeoutId exists. If it does,
+      // we will stop it.
+    if (addedMessageTimeoutId) {
+      clearTimeout(addedMessageTimeoutId);
+    }
+
+    const timeoutId = setTimeout(() => {
+    addedMessage.classList.remove('added-to-cart-visible');
     }, 2000);
+    // Save the timeoutId so we can stop it later.
+    addedMessageTimeoutId = timeoutId;
   });
 })
