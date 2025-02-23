ddaa825751038a473db2db40b2bd7f609ccd9835
fatemeh <fatemehsoleymani90@yahoo.com>
2025-02-23 12:31:53 -0500

    setTimeOut for removin message "added"

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -92,5 +92,9 @@ document.querySelectorAll('.js-add-to-cart').forEach((button) => {
     const addedMessage = document.querySelector(`.js-added-to-cart-${productId}`);
 
     addedMessage.classList.add('added-to-cart-visible');
+
+    setTimeout(() => {
+      addedMessage.classList.remove('added-to-cart-visible');
+    }, 2000);
   });
 })
