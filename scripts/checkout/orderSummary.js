b70d2962659daffca9729a111299a7ab1538e972
fatemeh <fatemehsoleymani90@yahoo.com>
2025-03-11 10:10:51 -0400

    make interactive delet button in order summary

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -3,6 +3,7 @@ import { products, getProduct } from "../../data/products.js";
 import { formatCurrency } from "../utils/money.js";
 import dayjs from " https://unpkg.com/dayjs@1.11.10/esm/index.js";
 import { deliveryOptions, getDeliveryOption } from "../../data/deliveryOptions.js";
+import { renderPaymentSummary } from "./paymentSummary.js";
 
 const today = dayjs();
 const deliveryDate = today.add(7, 'days');
@@ -138,7 +139,7 @@ export function renderOrderSummary() {
 
       container.remove();
 
-      updateCartQuantity();
+      renderPaymentSummary();
     });
   });
 
