0f60aa09b655761ee56d4168001f6a4b58395b3d
fatemeh <fatemehsoleymani90@yahoo.com>
2025-03-28 11:20:47 -0400

    add afterEach hook in orderSummaryTest.js

--- tests-jasmine/checkout/orderSummaryTest.js
+++ tests-jasmine/checkout/orderSummaryTest.js
@@ -32,6 +32,10 @@ describe('test suite: renderOrderSummary', () => {
     renderOrderSummary();
   });
 
+  afterEach(() => {
+    document.querySelector('.js-test-container').innerHTML = '';
+  });
+
   it('displays the cart', () => {
 
     expect(
@@ -44,10 +48,7 @@ describe('test suite: renderOrderSummary', () => {
 
     expect(
       document.querySelector(`.js-product-quantity-${productId2}`).innerText
-    ).toContain('Quantity: 1');
-
-    document.querySelector('.js-test-container').innerHTML = '';
-     
+    ).toContain('Quantity: 1');     
   });
 
   it('removes a product:', () => {
@@ -65,10 +66,7 @@ describe('test suite: renderOrderSummary', () => {
     expect(
       document.querySelector(`.js-cart-item-container-${productId2}`)
     ).not.toEqual(null);
-
     expect(cart.length).toEqual(1);
     expect(cart[0].productId).toEqual(productId2);
-
-    document.querySelector('.js-test-container').innerHTML = '';
   });
 });
