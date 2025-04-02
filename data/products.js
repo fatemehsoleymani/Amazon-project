c26c9ed81919662fdfc77c799ea581cb29b08d34
fatemeh <fatemehsoleymani90@yahoo.com>
2025-04-02 15:20:04 -0400

    Use inheritance and polymorghism to add size chart

--- data/products.js
+++ data/products.js
@@ -35,6 +35,26 @@ class Product {
     return `$${formatCurrency(this.priceCents)}`
   }
 
+  extraInfoHTML() {
+    return '';
+  }
+}
+
+class Clothing extends Product {
+  sizeChartLink;
+
+  constructor(productDetails) {
+    super(productDetails);
+    this.sizeChartLink = productDetails.sizeChartLink;
+  }
+
+  extraInfoHTML() {
+    return `
+      <a href="${this.sizeChartLink}" target="_blank">
+        Size chart
+      </a>
+    `;
+  }
 }
 
 export const products = [
@@ -717,5 +737,8 @@ export const products = [
     priceCents: 2999
   }
 ].map((productDetails) => {
+  if(productDetails.type === 'clothing') {
+    return new Clothing(productDetails)
+  }
   return new Product(productDetails);
 });
