e531a9a0e22404e471798af4bb066076d1b13dec
fatemeh <fatemehsoleymani90@yahoo.com>
2025-04-03 20:01:41 -0400

    Load products from backend

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,126 +1,131 @@
 import { cart, addToCart, calculateCartQuantity} from "../data/cart.js";
-import { products } from "../data/products.js";
+import { products, loadProducts } from "../data/products.js";
 import { formatCurrency } from "./utils/money.js";
 
-let productHTML = '';
+loadProducts(renderProductsGrid);
 
-products.forEach((product) => {
-  productHTML = productHTML +=  `
-        <div class="product-container">
-          <div class="product-image-container">
-            <img class="product-image"
-              src="${product.image}">
-          </div>
+function renderProductsGrid() {
 
-          <div class="product-name limit-text-to-2-lines">
-            ${product.name}
-          </div>
+  let productHTML = '';
 
-          <div class="product-rating-container">
-            <img class="product-rating-stars"
-              src="${product.getStarsUrl()}">
-            <div class="product-rating-count link-primary">
-              ${product.rating.count}
+  products.forEach((product) => {
+    productHTML = productHTML +=  `
+          <div class="product-container">
+            <div class="product-image-container">
+              <img class="product-image"
+                src="${product.image}">
             </div>
+
+            <div class="product-name limit-text-to-2-lines">
+              ${product.name}
+            </div>
+
+            <div class="product-rating-container">
+              <img class="product-rating-stars"
+                src="${product.getStarsUrl()}">
+              <div class="product-rating-count link-primary">
+                ${product.rating.count}
+              </div>
+            </div>
+
+            <div class="product-price">
+              ${product.getPrice()}
+            </div>
+
+            <div class="product-quantity-container">
+              <select class="js-quantity-selector-${product.id}">
+                <option selected value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+                <option value="5">5</option>
+                <option value="6">6</option>
+                <option value="7">7</option>
+                <option value="8">8</option>
+                <option value="9">9</option>
+                <option value="10">10</option>
+              </select>
+            </div>
+
+            ${product.extraInfoHTML()}
+
+            <div class="product-spacer"></div>
+
+            <div class="added-to-cart js-added-to-cart-${product.id}">
+              <img src="images/icons/checkmark.png">
+              Added
+            </div>
+
+            <button class="add-to-cart-button button-primary js-add-to-cart" data-product-id="${product.id}">
+              Add to Cart
+            </button>
           </div>
-
-          <div class="product-price">
-            ${product.getPrice()}
-          </div>
-
-          <div class="product-quantity-container">
-            <select class="js-quantity-selector-${product.id}">
-              <option selected value="1">1</option>
-              <option value="2">2</option>
-              <option value="3">3</option>
-              <option value="4">4</option>
-              <option value="5">5</option>
-              <option value="6">6</option>
-              <option value="7">7</option>
-              <option value="8">8</option>
-              <option value="9">9</option>
-              <option value="10">10</option>
-            </select>
-          </div>
-
-          ${product.extraInfoHTML()}
-
-          <div class="product-spacer"></div>
-
-          <div class="added-to-cart js-added-to-cart-${product.id}">
-            <img src="images/icons/checkmark.png">
-            Added
-          </div>
-
-          <button class="add-to-cart-button button-primary js-add-to-cart" data-product-id="${product.id}">
-            Add to Cart
-          </button>
-        </div>
-  `
-})
+    `
+  })
 
 
-document.querySelector('.js-products-grid').innerHTML = productHTML;
+  document.querySelector('.js-products-grid').innerHTML = productHTML;
 
 
-function updateCartQuantity() {
-  const cartQuantity = calculateCartQuantity();
-  document.querySelector('.js-cart-quantity')
-    .innerHTML = cartQuantity;
-}
-
-updateCartQuantity();
-
-document.querySelectorAll('.js-add-to-cart').forEach((button) => {
-  // closure. Each time we run the loop, it will create
-  // a new variable called addedMessageTimeoutId and do
-  // button.addEventListener().
-  //
-  // Then, because of closure, the function we give to
-  // button.addEventListener() will get a unique copy
-  // of the addedMessageTimeoutId variable and it will
-  // keep this copy of the variable forever.
-  // (Reminder: closure = if a function has access to a
-  // value/variable, it will always have access to that
-  // value/variable).
-  //
-  // This allows us to create many unique copies of the
-  // addedMessageTimeoutId variable (one for every time
-  // we run the loop) so it lets us keep track of many
-  // timeoutIds (one for each product).
-
-  let addedMessageTimeoutId;
-
-  function updateCartQuantity(productId) {
+  function updateCartQuantity() {
     const cartQuantity = calculateCartQuantity();
-
-    document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
-
-
-    const addedMessage = document.querySelector(`.js-added-to-cart-${productId}`);
-
-    addedMessage.classList.add('added-to-cart-visible');
-
-    // Check if a previous timeoutId exists. If it does,
-    // we will stop it.
-
-    if (addedMessageTimeoutId) {
-      clearTimeout(addedMessageTimeoutId);
-    }
-
-    const timeoutId = setTimeout(() => {
-    addedMessage.classList.remove('added-to-cart-visible');
-    }, 2000);
-    // Save the timeoutId so we can stop it later.
-    
-    addedMessageTimeoutId = timeoutId;
+    document.querySelector('.js-cart-quantity')
+      .innerHTML = cartQuantity;
   }
 
-  button.addEventListener('click', () => {
-    const {productId} = button.dataset;
+  updateCartQuantity();
 
-    addToCart(productId);
-    updateCartQuantity(productId);
-    
+  document.querySelectorAll('.js-add-to-cart').forEach((button) => {
+    // closure. Each time we run the loop, it will create
+    // a new variable called addedMessageTimeoutId and do
+    // button.addEventListener().
+    //
+    // Then, because of closure, the function we give to
+    // button.addEventListener() will get a unique copy
+    // of the addedMessageTimeoutId variable and it will
+    // keep this copy of the variable forever.
+    // (Reminder: closure = if a function has access to a
+    // value/variable, it will always have access to that
+    // value/variable).
+    //
+    // This allows us to create many unique copies of the
+    // addedMessageTimeoutId variable (one for every time
+    // we run the loop) so it lets us keep track of many
+    // timeoutIds (one for each product).
+
+    let addedMessageTimeoutId;
+
+    function updateCartQuantity(productId) {
+      const cartQuantity = calculateCartQuantity();
+
+      document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
+
+
+      const addedMessage = document.querySelector(`.js-added-to-cart-${productId}`);
+
+      addedMessage.classList.add('added-to-cart-visible');
+
+      // Check if a previous timeoutId exists. If it does,
+      // we will stop it.
+
+      if (addedMessageTimeoutId) {
+        clearTimeout(addedMessageTimeoutId);
+      }
+
+      const timeoutId = setTimeout(() => {
+      addedMessage.classList.remove('added-to-cart-visible');
+      }, 2000);
+      // Save the timeoutId so we can stop it later.
+      
+      addedMessageTimeoutId = timeoutId;
+    }
+
+    button.addEventListener('click', () => {
+      const {productId} = button.dataset;
+
+      addToCart(productId);
+      updateCartQuantity(productId);
+      
+    });
   });
-})
+}
